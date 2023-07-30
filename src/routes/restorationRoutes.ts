e4aecdb698d4958a72c5085aa8e2979223764504
Leo Gavin <leogavin123@outlook.com>
2023-07-30 10:03:17 +0530

    uppdate, get, get by id add to Restoration

--- src/routes/restorationRoutes.ts
+++ src/routes/restorationRoutes.ts
@@ -1,10 +1,13 @@
 // src/routes/restorationRoutes.ts
 import { Router } from 'express';
-import { createRestoration,upload } from '../controllers/RestorationController';
+import { createRestoration,upload,getRestorationById,getAllRestorations,updateRestoration } from '../controllers/RestorationController';
 
 const router = Router();
 
 // Create a new restoration vehicle
 router.post('/',upload.array('photos', 4), createRestoration);
+router.get('/:id', getRestorationById);
+router.get('/', getAllRestorations);
+router.put('/:id', updateRestoration);
 
 export default router;
